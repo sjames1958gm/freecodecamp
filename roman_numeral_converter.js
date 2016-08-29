7d667f2d66ed9bdabf4c150f9e48c80235ebfb6f
Stephen James <sajames1958@gmail.com>
2016-08-29 10:42:39 -0500

    update to more concise impl.

--- roman_numeral_converter.js
+++ roman_numeral_converter.js
@@ -2,50 +2,18 @@ function convertToRoman(num) {
   
   var result = "";
   
-  while (num > 0) {
-    if (num >= 1000) {
-      result += "M";
-      num -= 1000;
-    } else if (num > 900) {
-      result += "CM";
-      num -= 900;
-    } else if (num >= 500) {
-      result += "D";
-      num -= 500;
-    } else if (num > 400) {
-      result += "CD";
-      num -= 400;
-    } else if (num >= 100) {
-      result += "C";
-      num -= 100;
-    } else if (num > 90) {
-      result += "XC";
-      num -= 90;
-    } else if (num >= 50) {
-      result += "L";
-      num -= 50;
-    } else if (num > 40) {
-      result += "XL";
-      num -= 40;
-    } else if (num > 10) {
-      result += "X";
-      num -= 10;
-    } else if (num == 9) {
-      result += "IX";
-      num -= 9;
-    } else if (num >= 5) {
-      result += "V";
-      num -= 5;
-    } else if (num == 4) {
-      result += "IV";
-      num -= 4;
-    } else {
-      result += "I";
-      num -= 1;
-    }
-  }
+  var cvrt = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], 
+              [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], 
+              [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]];
   
- return result;
-}
+  var i = 0;
+  while (num > 0) {
+    if (num >= cvrt[i][0]) {
+      result += cvrt[i][1];
+      num -= cvrt[i][0];
+    }
+    else i++;
+  }
 
-``
+  return result;
+}
