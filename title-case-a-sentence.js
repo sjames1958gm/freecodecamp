3e63547935cd3f9eac0d2e3e4026a4579dbf300f
Stephen James <sajames1958@gmail.com>
2016-09-12 11:22:01 -0500

    Create title-case-a-sentence.js

---
+++ title-case-a-sentence.js
@@ -0,0 +1,9 @@
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(element){
+
+     return element[0].toUpperCase() + element.slice(1);
+  }).join(" ");
+  
+}
+
+titleCase("I'm a little tea pot");
