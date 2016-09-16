39b908bf4c99eae1830a9908ca35b1f485843f62
Stephen James <sajames1958@gmail.com>
2016-09-16 13:23:49 -0500

    Update everything-be-true.js

--- everything-be-true.js
+++ everything-be-true.js
@@ -1,10 +1,8 @@
 function truthCheck(collection, pre) {
   // Is everyone being true?
-  var result = collection.filter(function(ent) {
-    return !(ent[pre]);
-  });
-  
-  return result.length === 0;
+  return collection.filter(function(ent) {
+    return ent[pre];
+  }).length === collection.length;
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, 
