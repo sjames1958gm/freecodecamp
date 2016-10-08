e6a82e46b4b3de604c78803ce418873ec5f15cb6
Stephen James <sajames1958@gmail.com>
2016-10-08 09:21:25 -0500

    Create perm-alone.js

---
+++ perm-alone.js
@@ -0,0 +1,33 @@
+function permutate(arr1, arr2) {
+  if (arr2.length === 1) {
+    return arr1.concat(arr2);
+  }
+  else {
+    var result = [];
+    arr2.forEach(function(item, index, arr) {
+      var newarr = arr.slice(0, index).concat(arr.slice(index + 1));
+      var perm = permutate(arr1.concat([item]), newarr);
+      if (Array.isArray(perm[0]))
+        result = result.concat(perm);
+      else
+        result.push(perm);
+    });
+    
+    return result;
+  }
+}
+
+function permAlone(str) {
+  
+  var arr = str.split("");
+  
+  var perms = permutate([], arr);
+
+  return perms
+    .map(function(p) { p.join(""); })
+    .reduce(function (prev, curr) {
+      return curr.match(/(.)\1/) ? prev : prev + 1;
+    }, 0); 
+}
+
+permAlone('aab');
