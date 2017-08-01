423aff6c18f2c74e9458339a6c5e880ef9fb113e
Stephen James <sajames1958@gmail.com>
2017-08-01 03:54:35 -0500

    Update caesers-cipher3.js

--- caesers-cipher3.js
+++ caesers-cipher3.js
@@ -1,15 +1,16 @@
 function rot13(str) { // LBH QVQ VG!
-  var A = "A".charCodeAt(0);
-  var Z = "Z".charCodeAt(0);
-  var M = "M".charCodeAt(0);
+  let A = "A".charCodeAt(0);
+  let Z = "Z".charCodeAt(0);
+  let shift = ((Z - A + 1) / 2)
+  let Mid = A + shift;
   
   return [].reduce.call(str, function(a, b) {
     var code = b.charCodeAt();
-    if (code >= A && code <= M) {
-      code += 13;
+    if (code >= A && code < Mid) {
+      code += shift;
     }
-    else if (code > M && code <=Z) {
-      code -=13;
+    else if (code >= Mid && code <=Z) {
+      code -= shift;
     }
     return a + String.fromCharCode(code);
   }, "");
