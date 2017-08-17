de930615da891bf165ce8ac7b93c0dce3505b295
Stephen James <sajames1958@gmail.com>
2017-08-17 09:13:06 -0500

    Create forEachDelayed.js

---
+++ forEachDelayed.js
@@ -0,0 +1,25 @@
+function forEachDelayed(delay, cb, done) {
+  let i = 0;
+  t = 0;
+  
+  let fn = () => { 
+    setTimeout(() => {
+      cb(this[i]);
+      i++;
+      if (i < this.length) {
+        t = delay;
+        fn();
+      } else {
+        if (done) done();
+      }
+    }, t); 
+  };
+  
+  fn();
+}
+
+Array.prototype.forEachDelayed = forEachDelayed;
+
+[1, 2, 3, 4].forEachDelayed(1000,
+                            (e) => { console.log(e); },
+                            () => (console.log("done")))
